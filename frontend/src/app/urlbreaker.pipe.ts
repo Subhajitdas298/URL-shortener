d8278485e0d0ab0fd164b040014b98d85a353055
Subhajitdas298 <subhajitdas298@gmail.com>
2019-02-22 10:16:43 +0530

    Update urlbreaker.pipe.ts

--- frontend/src/app/urlbreaker.pipe.ts
+++ frontend/src/app/urlbreaker.pipe.ts
@@ -8,7 +8,7 @@ export class URLBreakerPipe implements PipeTransform {
   transform(value: string): string {
     const parts = value.split('://');
     let url: string;
-    if (parts[1] != null) {
+    if (parts[1] !== undefined) {
       url = parts[0] + '://' + parts[1].replace(/\//g, '/\u200B');
     } else {
       url =  parts[0].replace(/\//g, '/\u200B');
