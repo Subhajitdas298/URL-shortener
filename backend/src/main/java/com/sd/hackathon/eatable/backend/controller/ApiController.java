1a54de4c3367c883f52feb5f1bb0ff3742b3376f
Subhajitdas298 <subhajitdas298@gmail.com>
2019-02-22 17:29:06 +0530

    Update ApiController.java

--- backend/src/main/java/com/sd/hackathon/eatable/backend/controller/ApiController.java
+++ backend/src/main/java/com/sd/hackathon/eatable/backend/controller/ApiController.java
@@ -3,7 +3,6 @@ package com.sd.hackathon.eatable.backend.controller;
 import com.sd.hackathon.eatable.backend.document.UrlCode;
 import com.sd.hackathon.eatable.backend.service.urlconverter.UrlConverterService;
 
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
@@ -19,9 +18,6 @@ import java.net.URL;
 @RequestMapping("${application.api.root}")
 @CrossOrigin
 public class ApiController {
-
-    @Autowired
-    Logger logger;
 
     @Value("${application.api.root}")
     private String apiRoot;
