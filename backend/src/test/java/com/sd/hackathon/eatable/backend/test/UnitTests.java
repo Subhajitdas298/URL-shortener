7a6db591a4aad0c729d82a0613f4ff3e58c81091
Subhajitdas298 <subhajitdas298@gmail.com>
2019-02-22 17:47:16 +0530

    Fixed heroku config problem

--- backend/src/test/java/com/sd/hackathon/eatable/backend/test/UnitTests.java
+++ backend/src/test/java/com/sd/hackathon/eatable/backend/test/UnitTests.java
@@ -8,6 +8,7 @@ import com.sd.hackathon.eatable.backend.util.ValueCharConverter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,6 +16,7 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("local")
 public class UnitTests {
     /*
      Transcoder testing
